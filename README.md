e1c96a8bf8ce1639d9cb36e134bab9a20d0ccd72
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2024-01-25 20:15:43 -0600

    Update project.clj and README

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # http-tool
 
+[![Clojars Project](https://img.shields.io/clojars/v/com.github.paytonward6/http-tool.svg)](https://clojars.org/com.github.paytonward6/http-tool)
+
+Clojure HTTP client using only java.net
+
+## Installation
+
+### Leiningen/Boot
+
+`[com.github.paytonward6/http-tool "0.1.1"]`
+
+### Clojure CLI/deps.edn
+
+`com.github.paytonward6/http-tool {:mvn/version "0.1.1"}`
+
+### Gradle
+
+`implementation("com.github.paytonward6:http-tool:0.1.1")`
+
+
+## Usage
+
+```clojure
+(require '[http-tool.client :as http])
+
+(def response (http/get-url "https://google.com"))
+
+;; Returns a hash map of:
+;;
+;;  {:body "..."
+;;   :url "https://google.com"
+;;   :status 200
+;;   :headers {:status 200 :content-type "text/html; charset=ISO-8859-1" ...}
+;;   :version 1.1}
+
+```
--- project.clj
+++ project.clj
@@ -1,13 +1,11 @@
-(defproject http-tool "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject com.github.paytonward6/http-tool "0.1.1"
+  :description "Clojure HTTP client using only java.net"
+  :url "https://github.com/paytonward6/http-tool"
+  :license {:name "MIT"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.5.0"]
                  [org.clojure/core.async "1.6.673"]]
-  :main ^:skip-aot http-tool.core
-  :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :dev {:require [[nrepl/nrepl "1.1"]
