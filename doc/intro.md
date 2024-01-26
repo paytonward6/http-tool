df0a2017d92256ba6c297560ffe9e4576e83a3a0
paytonward6 <72841140+paytonward6@users.noreply.github.com>
2024-01-25 19:07:44 -0600

    Update meta files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to http-tool
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/http_tool/core_test.clj
@@ -0,0 +1,7 @@
+(ns http-tool.core-test
+  (:require [clojure.test :refer :all]
+            [http-tool.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
